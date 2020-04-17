ba5836c11d6fdbdceaddff6ff03db41e6fbb7be2
toni Haydamous <tonihaydamous@hotmail.com>
2020-04-17 14:21:04 +0300

    initial commit

---
+++ Email Courses Checker/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Email_Courses_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbResponse = new System.Windows.Forms.TextBox();
+            this.btnGetCourses = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbMaxEmails = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbClassNumbers = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbEmail.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tbEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbEmail.Location = new System.Drawing.Point(145, 81);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(256, 13);
+            this.tbEmail.TabIndex = 0;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbPassword.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tbPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbPassword.Location = new System.Drawing.Point(145, 143);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(256, 13);
+            this.tbPassword.TabIndex = 1;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // tbResponse
+            // 
+            this.tbResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbResponse.BackColor = System.Drawing.SystemColors.Window;
+            this.tbResponse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbResponse.Location = new System.Drawing.Point(54, 254);
+            this.tbResponse.Multiline = true;
+            this.tbResponse.Name = "tbResponse";
+            this.tbResponse.ReadOnly = true;
+            this.tbResponse.Size = new System.Drawing.Size(828, 305);
+            this.tbResponse.TabIndex = 4;
+            // 
+            // btnGetCourses
+            // 
+            this.btnGetCourses.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnGetCourses.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnGetCourses.FlatAppearance.BorderSize = 0;
+            this.btnGetCourses.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGetCourses.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnGetCourses.Location = new System.Drawing.Point(371, 210);
+            this.btnGetCourses.Name = "btnGetCourses";
+            this.btnGetCourses.Size = new System.Drawing.Size(193, 23);
+            this.btnGetCourses.TabIndex = 5;
+            this.btnGetCourses.Text = "Get Courses";
+            this.btnGetCourses.UseVisualStyleBackColor = false;
+            this.btnGetCourses.Click += new System.EventHandler(this.btnGetCourses_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(441, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Classes Numbers\r\n(seperated by commas)";
+            // 
+            // tbMaxEmails
+            // 
+            this.tbMaxEmails.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbMaxEmails.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tbMaxEmails.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbMaxEmails.Location = new System.Drawing.Point(633, 143);
+            this.tbMaxEmails.Name = "tbMaxEmails";
+            this.tbMaxEmails.Size = new System.Drawing.Size(256, 13);
+            this.tbMaxEmails.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(441, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 32);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Max Emails to Search\r\n(From newest to oldest)";
+            // 
+            // label5
+            // 
+            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+            this.label5.Location = new System.Drawing.Point(11, 4);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 64);
+            this.label5.TabIndex = 10;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label6.Image = ((System.Drawing.Image)(resources.GetObject("label6.Image")));
+            this.label6.Location = new System.Drawing.Point(383, 589);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(170, 27);
+            this.label6.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label7.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label7.Location = new System.Drawing.Point(145, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(256, 2);
+            this.label7.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label8.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label8.Location = new System.Drawing.Point(145, 92);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(256, 2);
+            this.label8.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label9.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label9.Location = new System.Drawing.Point(630, 154);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(256, 2);
+            this.label9.TabIndex = 14;
+            // 
+            // label10
+            // 
+            this.label10.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label10.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label10.Location = new System.Drawing.Point(630, 92);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(256, 2);
+            this.label10.TabIndex = 15;
+            // 
+            // tbClassNumbers
+            // 
+            this.tbClassNumbers.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbClassNumbers.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.tbClassNumbers.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbClassNumbers.Location = new System.Drawing.Point(633, 81);
+            this.tbClassNumbers.Name = "tbClassNumbers";
+            this.tbClassNumbers.Size = new System.Drawing.Size(256, 13);
+            this.tbClassNumbers.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(948, 620);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbMaxEmails);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbClassNumbers);
+            this.Controls.Add(this.btnGetCourses);
+            this.Controls.Add(this.tbResponse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbEmail);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Email Course Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbResponse;
+        private System.Windows.Forms.Button btnGetCourses;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbMaxEmails;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbClassNumbers;
+    }
+}
+
---
+++ Email Courses Checker/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Exchange.WebServices.Data;
+using HtmlAgilityPack;
+using System.IO;
+
+namespace Email_Courses_Checker
+{
+    public partial class Form1 : Form
+    {
+        ExchangeService exchange = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tbEmail.Text = "ID@ua.edu.lb";
+            tbMaxEmails.Text = "12";
+            StreamReader sr = new StreamReader(@".\CourseNumbers.txt");
+            tbClassNumbers.Text = sr.ReadLine();
+            sr.Close();
+        }
+
+        static bool RedirectionCallback(string url)
+        {
+            // Return true if the URL is an HTTPS URL.
+            return url.ToLower().StartsWith("https://");
+        }
+
+        private void btnGetCourses_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Initializing Service, Message
+                exchange = new ExchangeService(ExchangeVersion.Exchange2013);
+
+                //Defining Service Credentials
+                exchange.Credentials = new WebCredentials(tbEmail.Text, tbPassword.Text, "AutodiscoverUrl");
+                exchange.AutodiscoverUrl(tbEmail.Text, RedirectionCallback);
+
+                System.Threading.Tasks.Task.Run(() =>
+                {
+                    MessageBox.Show("Connected!\nFetching Email...");
+                });
+
+                GetEmails();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("The Autodiscover service couldn't be located."))
+                {
+                    MessageBox.Show("Wrong Email or Password");
+                    return;
+                }
+                MessageBox.Show(ex.Message + "\n\n" + ex);
+            }
+        }
+
+        private void GetEmails()
+        {
+            //Get Emails, specify the folder (Inbox) and the number of emails to get (100)
+            FindItemsResults<Item> findResults = exchange.FindItems(WellKnownFolderName.Inbox, new ItemView(Int32.Parse(tbMaxEmails.Text)));
+
+            bool foundCoursesEmail = false;
+
+            //Check for bounced emails
+            foreach (Item email in findResults.Items)
+            {
+                if (email.Subject.Contains("Online courses"))
+                {
+                    email.Load();
+                    string body = email.Body.Text;
+                    int Start = body.IndexOf("<table", 0);
+                    int End = body.IndexOf("</table>", Start) + "</table>".Length;
+                    string htmlTableString = body.Substring(Start, End - Start);
+                    DataTable TableCourses = ConvertHTMLTableToDataTable(htmlTableString);
+                    DataRow[] selectedRows = TableCourses.Select("[Class Nbr] IN (" + tbClassNumbers.Text + ")" , "Course ASC");
+
+                    tbResponse.Clear();
+                    tbResponse.Text = email.Subject;
+                    tbResponse.AppendText(Environment.NewLine);
+                    tbResponse.AppendText(Environment.NewLine);
+                    tbResponse.Text += "Your courses for this week are:";
+
+                    foreach (var row in selectedRows)
+                    {
+                        tbResponse.AppendText(Environment.NewLine);
+                        tbResponse.Text += row["Course"].ToString() + " - Instructor: " + row["First Name"].ToString() + " " + row["Last Name"].ToString();
+                    }
+                    foundCoursesEmail = true;
+                    break;
+                }
+            }
+            StreamWriter sw = new StreamWriter(@".\CourseNumbers.txt");
+            sw.WriteLine(tbClassNumbers.Text);
+            sw.Close();
+
+            //If online courses email was not found
+            if (!foundCoursesEmail)
+            {
+                tbResponse.Text = "The online courses email was not found, try putting a higher Max Emails number and try again.";
+            }
+        }
+
+        private DataTable ConvertHTMLTableToDataTable(string table)
+        {
+            DataTable output = new DataTable();
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(table);
+
+            var nodes = doc.DocumentNode.SelectNodes("//table//tr");
+
+            string textToGetHeaders = nodes[0].InnerText.Trim();
+
+            string[] stringSeparators = new string[] { "\r\n" };
+            var headers = textToGetHeaders.Split(stringSeparators, StringSplitOptions.None);
+            headers = headers.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            foreach (var header in headers)
+            {
+                output.Columns.Add(header);
+            }
+
+            var rows = nodes.Skip(1).Select(tr => tr
+                .Elements("td")
+                .Select(td => td.InnerText.Trim())
+                .ToArray());
+            foreach (var row in rows)
+            {
+                output.Rows.Add(row);
+            }
+            return output;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.kwalka.com");
+        }
+    }
+}
